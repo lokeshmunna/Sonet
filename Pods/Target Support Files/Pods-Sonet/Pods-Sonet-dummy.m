5a3c06e4b6907adfb51eb92d1321d6b20529a0a8
Vinay Kumar Darna <Vinay@ISECUREs-iMac.local>
2016-09-09 20:05:29 +0530

    UI Design

---
+++ Pods/Target Support Files/Pods-Sonet/Pods-Sonet-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sonet : NSObject
+@end
+@implementation PodsDummy_Pods_Sonet
+@end
---
+++ Sonet/LaunchVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Sonet
+//
+//  Created by Vinay Kumar Darna on 07/09/16.
+//  Copyright © 2016 iSecurePayments. All rights reserved.
+//
+
+import UIKit
+
+class LaunchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+
+}
+
